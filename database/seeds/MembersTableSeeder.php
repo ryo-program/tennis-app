7c84f57c9caeb1ed1b62b4dd10b46a114682c7ba
ryo-program <yh.1312.sun@gmail.com>
2020-07-02 12:46:55 +0900

    seed

--- database/seeds/MembersTableSeeder.php
+++ database/seeds/MembersTableSeeder.php
@@ -11,21 +11,21 @@ class MembersTableSeeder extends Seeder
             'year' => '1',
             'shot' => 'バックハンドのダウンザライン',
             'comment' => 'テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。',
-            'profile_img' => '',
+            // 'profile_img' => '',
         ]);
         DB::table('members')->insert([
             'name' => '錦織圭',
             'year' => '2',
             'shot' => 'バックハンドのダウンザライン',
             'comment' => 'テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。',
-            'profile_img' => '',
+            // 'profile_img' => '',
         ]);
         DB::table('members')->insert([
             'name' => '錦織圭',
             'year' => '3',
             'shot' => 'バックハンドのダウンザライン',
             'comment' => 'テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。テキスト。',
-            'profile_img' => '',
+            // 'profile_img' => '',
         ]);
     }
 }
